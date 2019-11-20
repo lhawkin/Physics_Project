9ee01906ed41c56017258d9a3a0b436292c9faf0
asadullahyousuf <52981929+asadullahyousuf@users.noreply.github.com>
2019-11-20 15:28:56 -0500

    finished

--- KinematicEquations.java
+++ KinematicEquations.java
@@ -1,8 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.InputMismatchException;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 class KinematicEquations implements requiredFunctions{
     private double deltaX = Double.NaN;
@@ -35,31 +31,21 @@ class KinematicEquations implements requiredFunctions{
         }
         this.setAsking_for(solve);
         String finalret = "";
-        double returned;
         switch (solve) {
             case 1:
                 finalret = "Displacement = " + this.solveDeltaX();
                 break;
             case 2:
-                this.setDeltaX();
-                this.setAcceleration();
-                this.setFinalVelocity();
-                this.setT();
+                finalret = "Initial Velocity = " + this.solveInitialVelocity();
                 break;
             case 3:
-                this.setDeltaX();
-                this.setAcceleration();
-                this.setVNot();;
-                this.setT();
+                finalret = "Final Velocity = " + this.solveFinalVelocity();
                 break;
             case 4:
-                finalret = "\nAcceleration = " + this.solveAcceleration();
+                finalret = "Acceleration = " + this.solveAcceleration();
                 break;
             case 5:
-                this.setDeltaX();
-                this.setAcceleration();
-                this.setFinalVelocity();
-                this.setVNot();
+                finalret = "Time Interval = " + this.solveTimeInterval();
                 break;
             default:
                 System.out.println("Nothing Happened.");
@@ -115,7 +101,7 @@ class KinematicEquations implements requiredFunctions{
         System.out.print("Enter Initial velocity value or copy and paste .10001011: ") ;
         double input = Double.NaN;
         try{
-             input = scan.nextDouble();
+            input = scan.nextDouble();
             if (input == .10001011 || input == 10001011){
                 input = Double.NaN;
             }
@@ -214,22 +200,25 @@ class KinematicEquations implements requiredFunctions{
             System.out.println("-------------------ERROR--------------------");
             System.exit(1);
         }
-        if(whatsMissing.equals("Final Velocity")){
-            ret = this.getVNot() * this.getT() + 0.5 * (this.getAcceleration()*Math.pow(this.getT(),2));
-        }
-        else if(whatsMissing.equals("Acceleration")){
-            double firstHalf =this.getFinalVelocity() + this.getVNot();
-            ret = ( firstHalf / 2) * this.getT();
-        }
-        else if(whatsMissing.equals("Time Interval")){
-            ret = (
-                    (Math.pow(this.getFinalVelocity(),2)/(2*this.getAcceleration())) - (Math.pow(this.getVNot(),2)/(2*this.getAcceleration()))
-            );
-        }
-        else if(whatsMissing.equals("Initial Velocity")){
-            ret = this.getFinalVelocity() * this.getT() + 0.5 * (this.getAcceleration()*Math.pow(this.getT(),2));
-        } else{
-            System.out.println("Im screwed");
+        switch (whatsMissing) {
+            case "Final Velocity":
+                ret = this.getVNot() * this.getT() + 0.5 * (this.getAcceleration() * Math.pow(this.getT(), 2));
+                break;
+            case "Acceleration":
+                double firstHalf = this.getFinalVelocity() + this.getVNot();
+                ret = (firstHalf / 2) * this.getT();
+                break;
+            case "Time Interval":
+                ret = (
+                        (Math.pow(this.getFinalVelocity(), 2) / (2 * this.getAcceleration())) - (Math.pow(this.getVNot(), 2) / (2 * this.getAcceleration()))
+                );
+                break;
+            case "Initial Velocity":
+                ret = this.getFinalVelocity() * this.getT() + 0.5 * (this.getAcceleration() * Math.pow(this.getT(), 2));
+                break;
+            default:
+                System.out.println("Im screwed");
+                break;
         }
 
         return ret;
@@ -258,31 +247,244 @@ class KinematicEquations implements requiredFunctions{
             System.exit(1);
         }
 
-        if(whatsMissing.equals("Displacement")){
-            ret = ((this.getFinalVelocity()/this.getT()) - (this.getVNot()/this.getT()));
-        }
-        else if(whatsMissing.equals("Final Velocity")){
-            double firstHalf =(2*this.getDeltaX())/Math.pow(this.getT(),2);
-            double secondHalf = (2*this.getVNot()) / this.getT();
-            ret = firstHalf - secondHalf;
-        }
-        else if(whatsMissing.equals("Time Interval")){
-            double firstHalf =(Math.pow(this.getFinalVelocity(),2) / (2*this.getDeltaX()));
-            double secondHalf =(Math.pow(this.getVNot(),2) / (2*this.getDeltaX()));
-            ret = firstHalf - secondHalf;
-        }
-        else if(whatsMissing.equals("Initial Velocity")){
-            double firstHalf =(2*this.getDeltaX())/Math.pow(this.getT(),2);
-            double secondHalf = (2*this.getFinalVelocity()) / this.getT();
-            ret = firstHalf - secondHalf;
+        switch (whatsMissing) {
+            case "Displacement":
+                ret = ((this.getFinalVelocity() / this.getT()) - (this.getVNot() / this.getT()));
+                break;
+            case "Final Velocity": {
+                double firstHalf = (2 * this.getDeltaX()) / Math.pow(this.getT(), 2);
+                double secondHalf = (2 * this.getVNot()) / this.getT();
+                ret = firstHalf - secondHalf;
+                break;
+            }
+            case "Time Interval": {
+                double firstHalf = (Math.pow(this.getFinalVelocity(), 2) / (2 * this.getDeltaX()));
+                double secondHalf = (Math.pow(this.getVNot(), 2) / (2 * this.getDeltaX()));
+                ret = firstHalf - secondHalf;
+                break;
+            }
+            case "Initial Velocity": {
+                double firstHalf = (2 * this.getDeltaX()) / Math.pow(this.getT(), 2);
+                double secondHalf = (2 * this.getFinalVelocity()) / this.getT();
+                ret = firstHalf - secondHalf;
 
-        } else{
-            System.out.println("Im screwed");
+                break;
+            }
+            default:
+                System.out.println("Im screwed");
+                break;
         }
 
         return ret;
     }
 
+    public double solveInitialVelocity(){
+        double ret = 0;
+        this.setDeltaX();
+        this.setAcceleration();
+        this.setFinalVelocity();
+        this.setT();
+        HashMap<Object, Object> map = this.getAllVariables();
+        String whatsMissing = "";
+        int counter = 0;
+        for (Map.Entry mapElement : map.entrySet()) {
+            if(mapElement.getValue().equals(Double.NaN)) {
+                if (mapElement.getKey() != this.getAsking_for()){
+                    whatsMissing = (String) mapElement.getKey();
+                }
+                counter++;
+            }
+        }
+        if(counter != 2){
+            System.out.println("\n-------------------ERROR-------------------");
+            System.out.println("You gave the calculator too many or too little numbers. Your question should only give you 3 three variables. Please start from the top.");
+            System.out.println("-------------------ERROR--------------------");
+            System.exit(1);
+        }
+
+        switch (whatsMissing) {
+            case "Displacement":
+                ret = (this.getFinalVelocity() - (this.getAcceleration()*this.getT()));
+                break;
+
+            case "Final Velocity": {
+                double firstHalf = this.getDeltaX()/this.getT();
+                double secondHalf = (this.getAcceleration() * this.getT())/2;
+                ret = firstHalf - secondHalf;
+                break;
+            }
+            case "Time Interval": {
+                double quadraticEquationOne = Math.sqrt((Math.pow(this.getFinalVelocity(),2)-(2*(this.getAcceleration()*this.getDeltaX()))));
+                double quadraticEquationTwo = -1*quadraticEquationOne;
+                if (quadraticEquationOne < 0 || quadraticEquationTwo < 0){
+                    ret = Math.max(quadraticEquationOne,quadraticEquationTwo);
+                }
+                if (quadraticEquationOne > 0 && quadraticEquationTwo > 0){
+                    ret = Math.min(quadraticEquationOne,quadraticEquationTwo);
+                }
+                break;
+            }
+            case "Acceleration": {
+                double firstHalf = (2*this.getDeltaX())/this.getT();
+                double secondHalf = this.getFinalVelocity();
+                ret = firstHalf - secondHalf;
+                break;
+            }
+            default:
+                System.out.println("Im screwed");
+                break;
+        }
+
+        return ret;
+    }
+
+    public double solveFinalVelocity(){
+        double ret = 0;
+        this.setDeltaX();
+        this.setAcceleration();
+        this.setVNot();;
+        this.setT();
+        HashMap<Object, Object> map = this.getAllVariables();
+        String whatsMissing = "";
+        int counter = 0;
+        for (Map.Entry mapElement : map.entrySet()) {
+            if(mapElement.getValue().equals(Double.NaN)) {
+                if (mapElement.getKey() != this.getAsking_for()){
+                    whatsMissing = (String) mapElement.getKey();
+                }
+                counter++;
+            }
+        }
+        if(counter != 2){
+            System.out.println("\n-------------------ERROR-------------------");
+            System.out.println("You gave the calculator too many or too little numbers. Your question should only give you 3 three variables. Please start from the top.");
+            System.out.println("-------------------ERROR--------------------");
+            System.exit(1);
+        }
+
+        switch (whatsMissing) {
+            case "Displacement":
+                ret = this.getVNot() + (this.getAcceleration()*this.getT());
+                break;
+
+            case "Initial Velocity": {
+                double firstHalf = this.getDeltaX()/this.getT();
+                double secondHalf = (this.getAcceleration() * this.getT())/2;
+                ret = firstHalf + secondHalf;
+                break;
+            }
+            case "Time Interval": {
+                double quadraticEquationOne = Math.sqrt((Math.pow(this.getVNot(),2)-(2*(this.getAcceleration()*this.getDeltaX()))));
+                double quadraticEquationTwo = -1*quadraticEquationOne;
+                if (quadraticEquationOne < 0 || quadraticEquationTwo < 0){
+                    ret = Math.max(quadraticEquationOne,quadraticEquationTwo);
+                }
+                if (quadraticEquationOne > 0 && quadraticEquationTwo > 0){
+                    ret = Math.min(quadraticEquationOne,quadraticEquationTwo);
+                }
+                break;
+            }
+            case "Acceleration": {
+                double firstHalf = (2*this.getDeltaX())/this.getT();
+                double secondHalf = this.getVNot();
+                ret = firstHalf - secondHalf;
+                break;
+            }
+            default:
+                System.out.println("Im screwed");
+                break;
+        }
+
+        return ret;
+    }
+
+    public double solveTimeInterval(){
+        double ret = 0;
+        this.setDeltaX();
+        this.setAcceleration();
+        this.setFinalVelocity();
+        this.setVNot();
+        HashMap<Object, Object> map = this.getAllVariables();
+        String whatsMissing = "";
+        int counter = 0;
+        for (Map.Entry mapElement : map.entrySet()) {
+            if(mapElement.getValue().equals(Double.NaN)) {
+                if (mapElement.getKey() != this.getAsking_for()){
+                    whatsMissing = (String) mapElement.getKey();
+                }
+                counter++;
+            }
+        }
+        if(counter != 2){
+            System.out.println("\n-------------------ERROR-------------------");
+            System.out.println("You gave the calculator too many or too little numbers. Your question should only give you 3 three variables. Please start from the top.");
+            System.out.println("-------------------ERROR--------------------");
+            System.exit(1);
+        }
+
+        switch (whatsMissing) {
+            case "Displacement":
+                ret = (this.getFinalVelocity()/this.getAcceleration()) - (this.getVNot()/this.getAcceleration());
+                break;
+
+            case "Final Velocity": {
+                double a = 0.5*this.getAcceleration();
+                double b = this.getVNot();
+                double c = this.getDeltaX()*-1;
+                double[] secondHalf = this.quadraticFormula(a,b,c);
+                if (secondHalf[0] < 0 || secondHalf[1] < 0){
+                    ret = Math.max(secondHalf[0],secondHalf[1]);
+                }
+                if (secondHalf[0] > 0 && secondHalf[1] > 0){
+                    ret = Math.min(secondHalf[0],secondHalf[1]);
+                }
+                break;
+            }
+            case "Initial Velocity": {
+                double a = 0.5*this.getAcceleration();
+                double b = this.getFinalVelocity();
+                double c = this.getDeltaX();
+                double[] secondHalf = this.quadraticFormula(a,b,c);
+                if (secondHalf[0] < 0 || secondHalf[1] < 0){
+                    ret = Math.max(secondHalf[0],secondHalf[1]);
+                }
+                if (secondHalf[0] > 0 && secondHalf[1] > 0){
+                    ret = Math.min(secondHalf[0],secondHalf[1]);
+                }
+                break;
+            }
+            case "Acceleration": {
+                ret = (2*this.getDeltaX()/(this.getVNot()+this.getFinalVelocity()));
+                break;
+            }
+            default:
+                System.out.println("Im screwed");
+                break;
+        }
+
+        return ret;
+    }
+
+    public double[] quadraticFormula(double a, double b, double c){
+        double[] roots = {0,0};
+        double root1, root2;
+        double determinant = b * b - 4 * a * c;
+        // condition for real and different roots
+        if(determinant > 0) {
+            root1 = (-b + Math.sqrt(determinant)) / (2 * a);
+            root2 = (-b - Math.sqrt(determinant)) / (2 * a);
+            roots[0] = root1;
+            roots[1] = root2;
+        }
+        // Condition for real and equal roots
+        if(determinant == 0) {
+            root1 = root2 = -b / (2 * a);
+            roots[0] = root1;
+            roots[1] = root1;
+        }
+        // If roots are not real
+        return roots;
+    }
     public static void main(String[] args) {
         KinematicEquations eq = new KinematicEquations();
         System.out.println(eq.baseSolver());
